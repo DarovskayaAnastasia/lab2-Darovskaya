93fa13813a1ad198fdf1e01876ac05ccf5d61f37
Anastasia Darovskaya <stiltskenx@gmail.com>
2019-10-05 11:52:00 +0000

    Scripted auto-commit on change (2019-10-05 11:52:00) by gitwatch.sh

---
+++ src/Partitioner.java
@@ -0,0 +1,5 @@
+public class Partitioner extends Partitioner<K, V> {
+    public int getPartition(K key, V value,  int numReduceTasks) {
+        return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
+}
