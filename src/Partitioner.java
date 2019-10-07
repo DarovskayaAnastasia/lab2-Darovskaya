524c83ac81a1465107ed54e23365b34717dadc9d
Anastasia Darovskaya <stiltskenx@gmail.com>
2019-10-07 11:16:08 +0000

    Scripted auto-commit on change (2019-10-07 11:16:08) by gitwatch.sh

--- src/Partitioner.java
+++
@@ -1,7 +0,0 @@
-package org.apache.hadoop.mapreduce;
-
-public class Partitioner extends Partitioner<K, V> {
-    public int getPartition(K key, V value,  int numReduceTasks) {
-        return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
-    }
-}
--- src/WritableComparable.java
+++
@@ -1,7 +0,0 @@
-public class WritableComparable implements RawComparator {
-    //...
-
-    public int compare (WritableComparable a, WritableComparable b) {
-        return a.compareTo(b);
-    }
-}
