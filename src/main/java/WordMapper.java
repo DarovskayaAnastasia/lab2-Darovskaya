7e71d6e536a2181b5962023e07c0bb67756e1d68
Anastasia Darovskaya <stiltskenx@gmail.com>
2019-10-14 14:55:15 +0000

    Scripted auto-commit on change (2019-10-14 14:55:15) by gitwatch.sh

--- src/main/java/WordMapper.java
+++ src/main/java/WordMapper.java
@@ -1,10 +1,6 @@
 import java.io.IOException;
-import java.util.*;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
 public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
