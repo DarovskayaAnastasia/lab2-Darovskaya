99db319302ff42f3c6eb50e74ccc302bbcb63902
Anastasia Darovskaya <stiltskenx@gmail.com>
2019-10-15 13:47:04 +0000

    Scripted auto-commit on change (2019-10-15 13:47:04) by gitwatch.sh

--- src/main/java/WritableCSV.java
+++ src/main/java/WritableCSV.java
@@ -6,11 +6,11 @@ import java.util.Vector;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-public class WritableCSV  {
+public class WritableCSV {
     public static void main(String[] args) throws IOException {
-        Vector records = parseCSV();
+        Vector records = parseCSV("src/main/resources/L_AIRPORT_ID.csv");
     }
-    Vector parseCSV(String path) throws IOException {
+    static Vector parseCSV(String path) throws IOException {
         Vector records = new Vector();
         File source = new File(path);
         CSVParser parser = CSVParser.parse(source, StandardCharsets.UTF_8, CSVFormat.RFC4180);
