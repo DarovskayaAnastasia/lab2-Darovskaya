a8cb66ea8cfe5a82b39346685ee2940fd1c936e8
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 19:41:56 +0000

    Scripted auto-commit on change (2019-10-18 19:41:55) by gitwatch.sh

--- src/main/java/OnTimeMapper.java
+++ src/main/java/OnTimeMapper.java
@@ -1,14 +1,31 @@
 import java.io.IOException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class OnTimeMapper extends Mapper<Object, Text, Text, Text> {
     @Override
-    protected void map(Object key, Text value, Context context) throws
+    protected void map(LongWritable key, Text value, Context context) throws
             IOException, InterruptedException {
+        String line = value.toString();
+        String[] textPair = parseLine(line);
 
-        String record = value.toString();
-        String[] idDelay = ;
-        context.write(new Text(), new Text());
+
+    }
+
+    private static String[] parseLine(String line) throws IOException {
+
+        String[] textPair = {};
+
+        CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180);
+        for (CSVRecord record : parser) {
+            textPair[0] = record.get(0);
+            textPair[1] = record.get(1);
+        }
+
+        return textPair;
     }
 }
