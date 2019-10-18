6a873b53a1bb8509f966b3d8bf3c5ac3838c558d
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 19:32:28 +0000

    Scripted auto-commit on change (2019-10-18 19:32:28) by gitwatch.sh

--- src/main/java/OnTimeMapper.java
+++ src/main/java/OnTimeMapper.java
@@ -1,14 +1,14 @@
-//import java.io.IOException;
-//import org.apache.hadoop.io.*;
-//import org.apache.hadoop.mapreduce.Mapper;
-//
-//public class OnTimeMapper extends Mapper<Object, Text, Text, Text> {
-//    @Override
-//    protected void map(Object key, Text value, Context context) throws
-//            IOException, InterruptedException {
-//
-//        String record = value.toString();
-//        String[] idDelay = ;
-//        context.write(new Text(), new Text());
-//    }
-//}
+import java.io.IOException;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class OnTimeMapper extends Mapper<Object, Text, Text, Text> {
+    @Override
+    protected void map(Object key, Text value, Context context) throws
+            IOException, InterruptedException {
+
+        String record = value.toString();
+        String[] idDelay = ;
+        context.write(new Text(), new Text());
+    }
+}
