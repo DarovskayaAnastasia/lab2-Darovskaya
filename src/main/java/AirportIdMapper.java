966409d01d359e8b9827eea10ca7392e0a516e60
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 19:54:08 +0000

    Scripted auto-commit on change (2019-10-18 19:54:08) by gitwatch.sh

--- src/main/java/AirportIdMapper.java
+++ src/main/java/AirportIdMapper.java
@@ -13,7 +13,7 @@ public class AirportIdMapper extends Mapper<LongWritable, Text, TextPair, Text> 
         String line = value.toString();
         String[] keyValuePair = parseLine(line);
 
-        TextPair 
+        TextPair indicatoredKey
         context.write(, new Text(keyValuePair[1]));
     }
 
