14a959ddd1046f0b759b32da21a34507189220da
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 18:15:57 +0000

    Scripted auto-commit on change (2019-10-18 18:15:57) by gitwatch.sh

--- src/main/java/HashPartitioner.java
+++ src/main/java/HashPartitioner.java
@@ -4,4 +4,9 @@ public class HashPartitioner<K, V> extends Partitioner<K, V> {
     public int getPartition(TextPair key, V value, int numReduceTasks) {
         return (key.getID().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
+
+    @Override
+    public int getPartition(K k, V v, int i) {
+        return 0;
+    }
 }
