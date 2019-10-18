fc467e8c7fcad78607b1291739842b8645a8914d
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 16:52:53 +0000

    Scripted auto-commit on change (2019-10-18 16:52:53) by gitwatch.sh

--- src/main/java/HashPartitioner.java
+++ src/main/java/HashPartitioner.java
@@ -1,2 +1,7 @@
-public class HashPartitioner {
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class HashPartitioner<K, V> extends Partitioner<K, V> {
+    public int getPartition(K key, V value, int numReduceTasks) {
+        return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
 }
