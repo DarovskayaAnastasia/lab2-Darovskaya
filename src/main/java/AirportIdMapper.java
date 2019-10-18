be84238986f4a86dbd2bb4ad9cc3f0719e50d5db
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 19:22:20 +0000

    Scripted auto-commit on change (2019-10-18 19:22:20) by gitwatch.sh

--- src/main/java/AirportIdMapper.java
+++ src/main/java/AirportIdMapper.java
@@ -1,25 +1,29 @@
-//import java.io.File;
-//import java.io.IOException;
-//import java.nio.charset.StandardCharsets;
-//
-//import org.apache.commons.csv.CSVFormat;
-//import org.apache.commons.csv.CSVParser;
-//import org.apache.commons.csv.CSVRecord;
-//import org.apache.hadoop.io.*;
-//import org.apache.hadoop.mapreduce.Mapper;
-//
-//public class AirportIdMapper extends Mapper<LongWritable, Text, TextPair, Text> {
-//    @Override
-//    protected void map(Object key, Text value, Context context) throws
-//            IOException, InterruptedException {
-//        String line = value.toString();
-//        String[] record = parseLine(line);
-//    }
-//
-//    private String[] parseLine(String line) {
-//        String[] record = {};
-//
-//        return record;
-//    }
-//
-//}
+import java.io.IOException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirportIdMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(Object key, Text value, Context context) throws
+            IOException, InterruptedException {
+        String line = value.toString();
+        String[] textPair = parseLine(line);
+    }
+
+    private static String[] parseLine(String line) throws IOException {
+
+        String[] textPair = {};
+
+        CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180);
+        for (CSVRecord record : parser) {
+            textPair[0] = record.get[0];
+            textPair[1] = record.get[1];
+        }
+
+        return textPair;
+    }
+
+}
--- src/main/java/WritableCSV.java
+++ src/main/java/WritableCSV.java
@@ -31,24 +31,15 @@
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 public class WritableCSV {
     public static void main(String[] args) throws IOException {
         String line = "\"10001\",\"Afognak Lake, AK: Afognak Lake Airport\"";
 
         CSVParser airports = parseCSV(line);
-        for (CSVRecord airport : airports) {
-//            System.out.println("id: " + airport.get(0) + "; name: " + airport.get(1));
-        }
-
-//        CSVParser delays = parseCSV("src/main/resources/664600583_T_ONTIME_sample.csv");
-//        for (CSVRecord delay : delays) {
-//            System.out.println("id: " + delay.get(14) + "; dest " + delay.get(18));
-//        }
     }
+
     static CSVParser parseCSV(String line) throws IOException {
 
         CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180);
