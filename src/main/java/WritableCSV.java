aa9aae4b998d132570f5a532334df876aa2325b4
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 09:52:42 +0000

    Scripted auto-commit on change (2019-10-18 09:52:42) by gitwatch.sh

--- src/main/java/WritableCSV.java
+++ src/main/java/WritableCSV.java
@@ -8,20 +8,15 @@ import java.nio.charset.StandardCharsets;
 
 public class WritableCSV {
     public static void main(String[] args) throws IOException {
-        Vector records = parseCSV("src/main/resources/L_AIRPORT_ID.csv");
-        int length = records.size();
-        for (int i = 0; i < length; ++i) {
-            System.out.println();
+        CSVParser records = parseCSV("src/main/resources/L_AIRPORT_ID.csv");
+        for (CSVRecord record : records) {
+            System.out.println(record.get(1));
         }
     }
-    static Vector parseCSV(String path) throws IOException {
-        Vector records = new Vector();
+    static CSVParser parseCSV(String path) throws IOException {
         File source = new File(path);
         CSVParser parser = CSVParser.parse(source, StandardCharsets.UTF_8, CSVFormat.RFC4180);
-        for (CSVRecord record : parser) {
-            records.addElement(record);
-        }
 
-        return records;
+        return parser;
     }
 }
