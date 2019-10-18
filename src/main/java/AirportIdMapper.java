12f21b5499c24ce8abf97b5a2517ba721beafdb1
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 10:30:24 +0000

    Scripted auto-commit on change (2019-10-18 10:30:24) by gitwatch.sh

--- src/main/java/AirportIdMapper.java
+++ src/main/java/AirportIdMapper.java
@@ -12,12 +12,12 @@ public class AirportIdMapper extends Mapper<Object, Text, Text, Text> {
     @Override
     protected void map(Object key, Text value, Context context) throws
             IOException, InterruptedException {
-//        String record = value.toString();
-        CSVParser records = parseCSV("src/main/resources/L_AIRPORT_ID.csv");
-        String parts[] = 
+        String record = value.toString();
+//        CSVParser records = parseCSV("src/main/resources/L_AIRPORT_ID.csv");
+//        String parts[] =
 
-//        String[] parts =
-//                record.split(",");
+        String[] parts =
+                record.split(",");
          context.write(new Text(parts[0]), new Text("airport_name    " + parts[1]));
     }
 
