cbe8c14c93fd2120f8442e7d2a6991c8d7ed3331
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 12:13:48 +0000

    Scripted auto-commit on change (2019-10-18 12:13:48) by gitwatch.sh

--- src/main/java/TextPair.java
+++ src/main/java/TextPair.java
@@ -1,3 +1,11 @@
-public class TextPair {
-    
+import org.apache.hadoop.io.WritableComparable;
+
+public class TextPair implements WritableComparable<TextPair> {
+
+    private int ID;
+    private byte Name;
+
+    public TextPair(int, byte) {
+        this.ID =
+    }
 }
