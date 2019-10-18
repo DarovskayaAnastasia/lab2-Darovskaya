90ec11d212c2cafc3910cc88dcb0db9cc786f885
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 19:01:58 +0000

    Scripted auto-commit on change (2019-10-18 19:01:58) by gitwatch.sh

--- src/main/java/AirportIdMapper.java
+++ src/main/java/AirportIdMapper.java
@@ -1,20 +1,25 @@
-//import java.io.File;
-//import java.io.IOException;
-//import java.nio.charset.StandardCharsets;
-//
-//import org.apache.commons.csv.CSVFormat;
-//import org.apache.commons.csv.CSVParser;
-//import org.apache.commons.csv.CSVRecord;
-//import org.apache.hadoop.io.*;
-//import org.apache.hadoop.mapreduce.Mapper;
-//
-//public class AirportIdMapper extends Mapper<LongWritable, Text, TextPair, Text> {
-//    @Override
-//    protected void map(Object key, Text value, Context context) throws
-//            IOException, InterruptedException {
-//        String line = value.toString();
-//        String[] record = parseLine(line);
-//    }
-//
-//
-//}
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirportIdMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(Object key, Text value, Context context) throws
+            IOException, InterruptedException {
+        String line = value.toString();
+        String[] record = parseLine(line);
+    }
+
+    private String[] parseLine(String line) {
+        String[] record = {};
+        
+        return record;
+    }
+
+}
--- src/main/java/JoinJob.java
+++ src/main/java/JoinJob.java
@@ -1,30 +1,30 @@
-//import org.apache.hadoop.fs.Path;
-//import org.apache.hadoop.io.*;
-//import org.apache.hadoop.mapreduce.Job;
-//import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-//import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-//import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-//import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-//
-//public class JoinJob {
-//    public static void main(String[] args) throws Exception {
-//        if (args.length != 2) {
-//            System.err.println("Usage: JoinJob <input path> <output path>");
-//            System.exit(-1);
-//        }
-//        Job job = Job.getInstance();
-//        job.setJarByClass(JoinJob.class);
-//        job.setJobName("JoinJob sort");
-//        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportIdMapper.class);
-//        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, OnTimeMapper.class);
-//        FileOutputFormat.setOutputPath(job, new Path(args[2]));
-//        job.setPartitionerClass(TextPair.HashPartitioner.class);
-//        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
-//        job.setReducerClass(JoinReducer.class);
-//        job.setMapOutputKeyClass(TextPair.class);
-//        job.setOutputKeyClass(Text.class);
-//        job.setOutputValueClass(Text.class);
-//        job.setNumReduceTasks(2);
-//        System.exit(job.waitForCompletion(true) ? 0 : 1);
-//    }
-//}
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class JoinJob {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: JoinJob <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportIdMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, OnTimeMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.HashPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
