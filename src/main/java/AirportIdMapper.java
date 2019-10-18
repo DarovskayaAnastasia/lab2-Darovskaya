25c320cc54a979a89ab0599726774d4c36e44fcc
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 09:53:08 +0000

    Scripted auto-commit on change (2019-10-18 09:53:08) by gitwatch.sh

--- src/main/java/AirportIdMapper.java
+++ src/main/java/AirportIdMapper.java
@@ -1,15 +1,14 @@
-import java.io.IOException;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.Mapper;
-
-tab
-public class AirportIdMapper extends Mapper<LongWritable, Text, Text, Text> {
-    @Override
-    protected void map(Object key, Text value, Context context) throws
-            IOException, InterruptedException {
-        String record = value.toString();
-        String[] parts =
-                record.split(",");
-         context.write(new Text(parts[0]), new Text("airport_name    " + parts[1]));
-    }
-}
+//import java.io.IOException;
+//import org.apache.hadoop.io.*;
+//import org.apache.hadoop.mapreduce.Mapper;
+//
+//public class AirportIdMapper extends Mapper<LongWritable, Text, Text, Text> {
+//    @Override
+//    protected void map(Object key, Text value, Context context) throws
+//            IOException, InterruptedException {
+//        String record = value.toString();
+//        String[] parts =
+//                record.split(",");
+//         context.write(new Text(parts[0]), new Text("airport_name    " + parts[1]));
+//    }
+//}
