6bc72947a61300fdfc8803cf514edd81b60458e8
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 16:49:50 +0000

    Scripted auto-commit on change (2019-10-18 16:49:50) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -1,2 +1,18 @@
-public class JoinReducer {
+import javafx.scene.text.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
+    }
 }
