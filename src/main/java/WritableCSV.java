285994d9b5c1ca7d52ae9573cb3157dc097b2c8a
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 18:58:34 +0000

    Scripted auto-commit on change (2019-10-18 18:58:34) by gitwatch.sh

--- src/main/java/WritableCSV.java
+++ src/main/java/WritableCSV.java
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets;
 
 public class WritableCSV {
     public static void main(String[] args) throws IOException {
-        CSVParser airports = parseCSV("src/main/resources/L_AIRPORT_ID.csv");
+        CSVParser airports = parseCSV("10001","Afognak Lake, AK: Afognak Lake Airport");
         for (CSVRecord airport : airports) {
 //            System.out.println("id: " + airport.get(0) + "; name: " + airport.get(1));
         }
@@ -17,9 +17,9 @@ public class WritableCSV {
 //            System.out.println("id: " + delay.get(14) + "; dest " + delay.get(18));
 //        }
     }
-    static CSVParser parseCSV(String path) throws IOException {
-        File source = new File(path);
-        CSVParser parser = CSVParser.parse(source, StandardCharsets.UTF_8, CSVFormat.RFC4180);
+    static CSVParser parseCSV(String line) throws IOException {
+
+        CSVParser parser = CSVParser.parse(line, StandardCharsets.UTF_8, CSVFormat.RFC4180);
         for (CSVRecord record : parser) {
             System.out.println(record);
         }
