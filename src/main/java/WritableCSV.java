a2bd5571e93289b9a2be767bb76bd0a3eb75c6c2
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 19:14:47 +0000

    Scripted auto-commit on change (2019-10-18 19:14:47) by gitwatch.sh

--- src/main/java/WritableCSV.java
+++ src/main/java/WritableCSV.java
@@ -1,3 +1,33 @@
+//import org.apache.commons.csv.CSVParser;
+//import org.apache.commons.csv.CSVFormat;
+//import org.apache.commons.csv.CSVRecord;
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//
+//public class WritableCSV {
+//    public static void main(String[] args) throws IOException {
+//        CSVParser airports = parseCSV("src/main/resources/L_AIRPORT_ID.csv");
+//        for (CSVRecord airport : airports) {
+////            System.out.println("id: " + airport.get(0) + "; name: " + airport.get(1));
+//        }
+//
+////        CSVParser delays = parseCSV("src/main/resources/664600583_T_ONTIME_sample.csv");
+////        for (CSVRecord delay : delays) {
+////            System.out.println("id: " + delay.get(14) + "; dest " + delay.get(18));
+////        }
+//    }
+//    static CSVParser parseCSV(String path) throws IOException {
+//        File source = new File(path);
+//        CSVParser parser = CSVParser.parse(source, StandardCharsets.UTF_8, CSVFormat.RFC4180);
+//        for (CSVRecord record : parser) {
+//            System.out.println(record);
+//        }
+//
+//        return parser;
+//    }
+//}
+
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -7,7 +37,9 @@ import java.nio.charset.StandardCharsets;
 
 public class WritableCSV {
     public static void main(String[] args) throws IOException {
-        CSVParser airports = parseCSV("src/main/resources/L_AIRPORT_ID.csv");
+        String line = " \"10001\",\"Afognak Lake, AK: Afognak Lake Airport\" ";
+
+        CSVParser airports = parseCSV(line);
         for (CSVRecord airport : airports) {
 //            System.out.println("id: " + airport.get(0) + "; name: " + airport.get(1));
         }
@@ -17,9 +49,9 @@ public class WritableCSV {
 //            System.out.println("id: " + delay.get(14) + "; dest " + delay.get(18));
 //        }
     }
-    static CSVParser parseCSV(String path) throws IOException {
-        File source = new File(path);
-        CSVParser parser = CSVParser.parse(source, StandardCharsets.UTF_8, CSVFormat.RFC4180);
+    static CSVParser parseCSV(String line) throws IOException {
+
+        CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180);
         for (CSVRecord record : parser) {
             System.out.println(record);
         }
@@ -27,3 +59,4 @@ public class WritableCSV {
         return parser;
     }
 }
+
