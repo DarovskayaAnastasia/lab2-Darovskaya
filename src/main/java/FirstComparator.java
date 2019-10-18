172e05d3f9bed37cc5c00aaa7ae55075a0ca57e3
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 20:07:59 +0000

    Scripted auto-commit on change (2019-10-18 20:07:59) by gitwatch.sh

--- src/main/java/FirstComparator.java
+++ src/main/java/FirstComparator.java
@@ -2,10 +2,6 @@ import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparable;
 
 public class FirstComparator implements RawComparator {
-//    @Override
-    public int compare(WritableComparable a, WritableComparable b) {
-        return a.compareTo(b);
-    }
 
     @Override
     public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
@@ -15,5 +11,9 @@ public class FirstComparator implements RawComparator {
     @Override
     public int compare(Object o, Object t1) {
         return 0;
+    }
+
+    public int compare(WritableComparable a, WritableComparable b) {
+        return a.compareTo(b);
     }
 }
