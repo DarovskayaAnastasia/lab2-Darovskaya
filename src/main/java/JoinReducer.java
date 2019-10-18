e5f6301154fa292666bb578c1b121d791322f688
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-18 20:38:21 +0000

    Scripted auto-commit on change (2019-10-18 20:38:21) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -1,6 +1,5 @@
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.VIntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.util.Iterator;
