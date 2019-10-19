caecd0e057e3d2af31670c4e9cd9a6e8ffa74e7c
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-19 09:43:09 +0000

    Scripted auto-commit on change (2019-10-19 09:43:09) by gitwatch.sh

--- src/main/java/OnTimeMapper.java
+++ src/main/java/OnTimeMapper.java
@@ -13,7 +13,7 @@ public class OnTimeMapper extends Mapper<LongWritable, Text, TextPair, Text> {
         String line = value.toString();
         String[] keyValuePair = parseLine(line);
 
-        if (keyValuePair[0] != "YEAR") {
+        if (keyValuePair[0] != "YEAR" && keyValuePair[1] != "") {
             TextPair indicatoredKey = new TextPair(Integer.parseInt(keyValuePair[0]), (byte) 1);
             context.write(indicatoredKey, new Text(keyValuePair[1]));
         }
