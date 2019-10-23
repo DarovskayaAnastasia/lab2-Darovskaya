88f7c4469b157e50e273be96c64f0a2a4907f9ec
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-23 07:37:14 +0000

    Scripted auto-commit on change (2019-10-23 07:37:14) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -1,43 +1,43 @@
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
-import java.io.IOException;
-import java.util.Iterator;
-
-public class JoinReducer extends Reducer<TextPair, Text, IntWritable, Text> {
-    @Override
-    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
-            IOException, InterruptedException {
-        Iterator<Text> iter = values.iterator();
-        Text name = new Text(iter.next());
-
-        float maxDelay = Float.MIN_VALUE;
-        float minDelay = Float.MAX_VALUE;
-        float averageDelay = 0;
-        int n = 0;
-
-        while (iter.hasNext()) {
-
-            n++;
-            float delay = Float.parseFloat(iter.next().toString());
-
-            if ( delay >= 0 ) {
-                averageDelay += delay;
-
-                if (delay > maxDelay) {
-                    maxDelay = delay;
-                } else if (delay < minDelay) {
-                    minDelay = delay;
-                }
-            }
-
-            averageDelay /= n;
-        }
-
-        context.write(new IntWritable(key.getID()), new Text(name + ": "
-                + "min delay time = " + minDelay
-                + "; max delay time = " + maxDelay
-                + "; average delay time = " + averageDelay
-                + ";"));
-    }
-}
+//import org.apache.hadoop.io.IntWritable;
+//import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.mapreduce.Reducer;
+//import java.io.IOException;
+//import java.util.Iterator;
+//
+//public class JoinReducer extends Reducer<TextPair, Text, IntWritable, Text> {
+//    @Override
+//    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+//            IOException, InterruptedException {
+//        Iterator<Text> iter = values.iterator();
+//        Text name = new Text(iter.next());
+//
+//        float maxDelay = Float.MIN_VALUE;
+//        float minDelay = Float.MAX_VALUE;
+//        float averageDelay = 0;
+//        int n = 0;
+//
+//        while (iter.hasNext()) {
+//
+//            n++;
+//            float delay = Float.parseFloat(iter.next().toString());
+//
+//            if ( delay >= 0 ) {
+//                averageDelay += delay;
+//
+//                if (delay > maxDelay) {
+//                    maxDelay = delay;
+//                } else if (delay < minDelay) {
+//                    minDelay = delay;
+//                }
+//            }
+//
+//            averageDelay /= n;
+//        }
+//
+//        context.write(new IntWritable(key.getID()), new Text(name + ": "
+//                + "min delay time = " + minDelay
+//                + "; max delay time = " + maxDelay
+//                + "; average delay time = " + averageDelay
+//                + ";"));
+//    }
+//}
--- src/main/java/OnTimeMapper.java
+++ src/main/java/OnTimeMapper.java
@@ -1,36 +1,36 @@
-import java.io.IOException;
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.Mapper;
-
-public class OnTimeMapper extends Mapper<LongWritable, Text, TextPair, Text> {
-    @Override
-    protected void map(LongWritable key, Text value, Context context) throws
-            IOException, InterruptedException {
-        String line = value.toString();
-        String[] keyValuePair = new String[2];
-        keyValuePair = parseLine(line);
-
-        if ((key.get() > 0) && keyValuePair[1] != "") {
-            TextPair indicatoredKey = new TextPair(Integer.parseInt(keyValuePair[0]), (byte) 1);
-            context.write(indicatoredKey, new Text(keyValuePair[1]));
-        }
-
-    }
-
-    private String[] parseLine(String line) throws IOException {
-
-        String[] result = new String[2];
-
-        CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180);
-        for (CSVRecord record : parser) {
-            result[0] = record.get(0);
-            result[1] = record.get(17);
-        }
-
-        return result;
-    }
-}
+//import java.io.IOException;
+//
+//import org.apache.commons.csv.CSVFormat;
+//import org.apache.commons.csv.CSVParser;
+//import org.apache.commons.csv.CSVRecord;
+//import org.apache.hadoop.io.*;
+//import org.apache.hadoop.mapreduce.Mapper;
+//
+//public class OnTimeMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+//    @Override
+//    protected void map(LongWritable key, Text value, Context context) throws
+//            IOException, InterruptedException {
+//        String line = value.toString();
+//        String[] keyValuePair = new String[2];
+//        keyValuePair = parseLine(line);
+//
+//        if ((key.get() > 0) && keyValuePair[1] != "") {
+//            TextPair indicatoredKey = new TextPair(Integer.parseInt(keyValuePair[0]), (byte) 1);
+//            context.write(indicatoredKey, new Text(keyValuePair[1]));
+//        }
+//
+//    }
+//
+//    private String[] parseLine(String line) throws IOException {
+//
+//        String[] result = new String[2];
+//
+//        CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180);
+//        for (CSVRecord record : parser) {
+//            result[0] = record.get(0);
+//            result[1] = record.get(17);
+//        }
+//
+//        return result;
+//    }
+//}
