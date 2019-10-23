2d575bcea53bbb507378e430671889c7a9dfd62e
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-23 09:10:08 +0000

    Scripted auto-commit on change (2019-10-23 09:10:08) by gitwatch.sh

--- src/main/java/FirstComparator.java
+++ src/main/java/FirstComparator.java
@@ -1,23 +1,28 @@
-import org.apache.hadoop.io.RawComparator;
+//import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 
-import static org.graalvm.compiler.word.BarrieredAccess.readInt;
 
-public class FirstComparator implements RawComparator {
+public class FirstComparator extends WritableComparator {
+
+    protected FirstComparator() {
+        super(TextPair.class, true);
+    }
+//
+//    @Override
+//    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+//        Integer i1 = readInt(b1, s1);
+//        Integer i2 = readInt(b2, s2);
+//
+//        return i1.compareTo(i2);
+//    }
+//
+//    @Override
+//    public int compare(Object o, Object t1) {
+//        return 0;
+//    }
 
     @Override
-    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-        Integer i1 = readInt(b1, s1);
-        Integer i2 = readInt(b2, s2);
-
-        return i1.compareTo(i2);
-    }
-
-    @Override
-    public int compare(Object o, Object t1) {
-        return 0;
-    }
-
     public int compare(WritableComparable a, WritableComparable b) {
         TextPair pair1 = (TextPair)a;
         TextPair pair2 = (TextPair)b;
