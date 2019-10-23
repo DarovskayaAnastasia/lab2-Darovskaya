a4107c8f34bce1682c7de8c71ed5ad65094ec164
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-10-23 07:30:45 +0000

    Scripted auto-commit on change (2019-10-23 07:30:45) by gitwatch.sh

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,2 +1,26 @@
+import java.io.IOException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
 public class TestClass {
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+    private String[] parseLine(String line) throws IOException {
+
+        String[] result = new String[2];
+
+        CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180);
+        for (CSVRecord record : parser) {
+            result[0] = record.get(0);
+            result[1] = record.get(17);
+        }
+
+        return result;
+    }
 }
